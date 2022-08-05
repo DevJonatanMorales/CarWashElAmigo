3ad905d80c1c9405c2752b09f2ef4e9bc085ada7
Jonatan Morales <developerjonatanmorales@gmail.com>
2022-08-05 00:12:44 -0300

    escaneo automatico

--- app/src/main/java/com/devjonatanmo/carwashelamigo/MainActivity.java
+++ app/src/main/java/com/devjonatanmo/carwashelamigo/MainActivity.java
@@ -39,12 +39,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-                IntentIntegrator integrator = new IntentIntegrator( MainActivity.this);
-                integrator.setDesiredBarcodeFormats(IntentIntegrator.ALL_CODE_TYPES);
-                integrator.setPrompt("Lector - Car Wash El Amigo");
-                integrator.setCameraId(0);
-                integrator.setBeepEnabled(true);
-                integrator.initiateScan();
+                Intent intent = new Intent(MainActivity.this, EscanearPlaca.class);
+                startActivity(intent);
 
             }
         });
@@ -57,100 +53,5 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-    }
-
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
-
-        if (result != null) {
-            if (result.getContents() == null) {
-                Toast.makeText(this, "Lectora cancelada", Toast.LENGTH_LONG).show();
-            } else {
-                BuscarPlaca(result.getContents());
-
-            }
-        } else {
-            super.onActivityResult(requestCode, resultCode, data);
-        }
-    }
-
-    //buscamos la placa en la bd
-
-    private void BuscarPlaca(String numPlaca) {
-        //obtenemos la fecha
-        Calendar c = Calendar.getInstance();
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        strDate = sdf.format(c.getTime());
-
-        BaseDatos root = new BaseDatos (MainActivity.this,"CarWash",null,1);
-        SQLiteDatabase BaseDatos = root.getWritableDatabase();
-
-        try {
-
-            //fila = BaseDatos.rawQuery("SELECT id_placa, placa, contador, fecha FROM t_placa INNER JOIN t_historial ON t_placa.id_placa=t_historial.fk_placa WHERE t_placa.placa='"+ numPlaca +"'", null);
-            fila = BaseDatos.rawQuery("SELECT id_placa, placa, contador, fecha " +
-                    "FROM t_placa INNER JOIN t_historial ON t_placa.id_placa=t_historial.fk_placa " +
-                    "WHERE t_placa.placa='"+ numPlaca +"'", null);
-            if (fila.moveToFirst() == true) {
-                idPlaca = fila.getInt(0);
-                placa = fila.getString(1);
-                contador = fila.getInt(2);
-
-                if (numPlaca.equals(placa)) {
-
-                    if (contador == 5) {
-                        accion = "reset";
-                    } else {
-                        accion = "update";
-                    }
-
-                    String historial = "";
-                    int item = 0;
-
-                    for (int i = 1; i <= fila.getCount(); i++ ){
-                        if (item == 0) {
-                            historial += "\nfecha de lavado: " + fila.getString(3);
-                            item = 1;
-
-                        }else if (fila.moveToNext()) {
-                            historial += "\nfecha de lavado: " + fila.getString(3);
-
-                        }
-
-                    }
-
-
-                    placaInfo = "Número de placa: " + placa + "\nfecha: " + strDate + "\nhistorial: " + historial;
-
-                }
-
-            } else {
-                placa = numPlaca;
-                placaInfo = "Número de placa: " + numPlaca + "\n" + //agregamos la descripcion
-                    "placa sin historial \nfecha: " + strDate;
-            }
-
-            EnviarDatos();
-
-        } catch (Exception ex) {
-            Toast.makeText(getApplicationContext(),"Error: " + ex.getMessage().toString(), Toast.LENGTH_LONG).show();
-        }
-    }
-
-    private void EnviarDatos () {
-        try {
-            Intent intent = new Intent( MainActivity.this, Factura.class);
-
-            intent.putExtra("numPlaca", placa);
-            intent.putExtra("idPlaca", idPlaca);
-            intent.putExtra("placaInfo", placaInfo);
-            intent.putExtra("accion", accion);
-            intent.putExtra("contador", contador);
-            intent.putExtra("nuencaFecha", strDate);
-
-            startActivity(intent);
-        } catch (Exception ex) {
-            Toast.makeText(getApplicationContext(),"Error al enviar: " + ex.getMessage().toString(), Toast.LENGTH_LONG).show();
-        }
     }
 }
