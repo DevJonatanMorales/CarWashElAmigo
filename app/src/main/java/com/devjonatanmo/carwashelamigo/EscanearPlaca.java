e76b1e0fe9a4184d674404bc8a52d67d108e0924
Jonatan Morales <developerjonatanmorales@gmail.com>
2022-08-05 00:03:57 -0300

    activity EscanearPlaca, EscaneoManual y EscaneoAutomatico

---
+++ app/src/main/java/com/devjonatanmo/carwashelamigo/EscanearPlaca.java
@@ -0,0 +1,14 @@
+package com.devjonatanmo.carwashelamigo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EscanearPlaca extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_escanear_placa);
+    }
+}
---
+++ app/src/main/java/com/devjonatanmo/carwashelamigo/EscaneoManual.java
@@ -0,0 +1,14 @@
+package com.devjonatanmo.carwashelamigo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EscaneoManual extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_escaneo_manual);
+    }
+}
